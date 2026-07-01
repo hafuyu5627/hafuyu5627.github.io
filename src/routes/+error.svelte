71f381bd4b0a0fcb1f08d9c9d2c06877ff04c188
Hafuyu5627 <298059101+hafuyu5627@users.noreply.github.com>
2026-07-01 09:19:49 +0800

    Add error page and fix layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	let { msg = page.error!.message, status = page.status.toString() } = $props()
+</script>
+
+<div class="flex flex-col h-full">
+	<div class="flex flex-col gap-[3%] items-center justify-center bg-blue-300 [flex:4]">
+		<div class="text-blue-700 font-bold text-7xl xl:text-9xl">
+			{status}
+		</div>
+
+		<div class="font-bold text-3xl xl:text-7xl lg:text-6xl md:text-5xl">
+			{msg}
+		</div>
+	</div>
+	<footer class="bg-blue-200 hover:bg-blue-100 flex-1 text-center relative flex text-gray-950 hover:text-gray-500 transition-colors">
+		<a class="text-3xl flex-1 content-center" href="/">返回主頁</a>
+	</footer>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 
 <!-- <svelte:head><link rel="icon" href={favicon} /></svelte:head> -->
 
-<header class="bg-blue-300 flex justify-between p-4 drop-shadow-md">
+<header class="bg-blue-300 flex justify-between p-4 drop-shadow-md sticky">
 	<a href="/" class="text-center content-center text-3xl text-black hover:text-white transition-colors">羽冬</a>
 	<nav class="flex *:px-4">
 		{#snippet link(path: string, name: string)}
@@ -24,4 +24,6 @@
 	</nav>
 </header>
 
-{@render children()}
+<main class="grow">
+	{@render children()}
+</main>
